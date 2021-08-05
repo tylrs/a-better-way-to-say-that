f03c5a119524d54f67b3bf79fbb8a89720375c4e
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-05 11:37:19 -0600

    Add basic hardcoded panel JSX

---
+++ src/components/Panel/Panel.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Panel = () => {
+    return (
+        <article>
+            <h2>Wow your sentence was</h2>
+            <p>Positive</p>
+            <hr />
+            <h3>Here are the positive words in your sentence:</h3>
+            <p>happy, bunnies</p>
+            <hr />
+            <h3>How would you like to change your sentence?</h3>
+            <button>More Positive</button>
+            <button>More Negative</button>
+            <button>Random</button>
+        </article>
+    )
+}
+
+export default Panel
