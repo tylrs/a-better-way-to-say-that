a1959ba30f5192dc02efa242da9a3f126e89316b
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-05 11:30:30 -0600

    Add basic PanelContainer setup

---
+++ src/components/PanelContainer/PanelContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Panel from '../Panel/Panel'
+
+
+const PanelContainer = () => {
+ 
+    return (
+        <article>
+            
+        </article>
+    )
+}
+
+export default PanelContainer
