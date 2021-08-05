1ab731c7c6b975a10f41396345e1f7c30957c748
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-05 11:40:23 -0600

    Add basic form JSX

---
+++ src/components/Form/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <form>
+            <label>Enter your sentence here</label>
+            <input type='text'></input>
+            <button>Submit</button>
+        </form>
+    )
+}
+
+export default Form
