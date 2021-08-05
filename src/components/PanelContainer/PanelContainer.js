775f9085bc73e1975e7fc9b69673b4e6d3c343e7
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-05 17:25:50 -0600

    Add alternative method of rendering panel content

--- src/components/PanelContainer/PanelContainer.js
+++ src/components/PanelContainer/PanelContainer.js
@@ -4,8 +4,28 @@ import Panel from '../Panel/Panel'
 import Form from '../Form/Form'
 
 
-const PanelContainer = () => {
- 
+const PanelContainer = (props) => {
+    let panel2 = [
+        {type: 'text', content: 'Wow your sentence was:'},
+        {type: 'result', content: 'positive'},
+        {type: 'text', content: 'Here are the positive words in your sentence:'},
+        {type: 'result', content: 'bunny, happy'},
+        {type: 'text', content: 'How would you like to change your sentence?'},
+        {type: 'button', content: 'More Positive'},
+        {type: 'button', content: 'More Negative'},
+        {type: 'button', content: 'Neutral'}
+    ]
+    let panel3 = [
+        {type: 'text', content: 'Here is your new sentence:'},
+        {type: 'result', content: props.result},
+        {type: 'button', content: 'Save this sentence'}
+    ]
+    let panel4 = [
+        {type: 'text', content: 'You can now:'},
+        {type: 'button', content: 'Change a new sentence'},
+        {type: 'button', content: 'Try again with same sentence'}
+    ]
+
     return (
         <section className='panel-container'>
             <Form />
