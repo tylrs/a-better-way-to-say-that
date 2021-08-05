21b17d2ac8dc716ddfb08cf9ad0d8304a023f53d
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-04 20:53:05 -0600

    import Form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,17 @@
-
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import Form from '../Form/Form'
 
 function App() {
   return (
-    <></>
+    <main>
+      <header>
+        <h1>A Better Way To Say That</h1>
+      </header>
+      <section>
+  
+      </section>
+    </main>
   );
 }
 
