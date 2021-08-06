10c1d3a2d11580e903b7249d5a56f0ef2e25032d
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-05 21:33:22 -0600

    Add function to convert sentiment to full word

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,13 +1,39 @@
 export const cleanSentimentAnalysis = (result) => {
-    const polarityTerms = result.sentence_list[0].segment_list[0].polarity_term_list.map(term => {
+    const sentiment = convertToFullSentiment(result.score_tag)
+    const polarityTerms = result.sentence_list[0].segment_list[0].polarity_term_list.map(word => {
+        let wordSentiment = convertToFullSentiment(word.score_tag)
         return {
-            text: term.text,
-            sentiment: term.score_tag
+            text: word.text,
+            wordSentiment
         }
     })
     return {
-        sentiment: result.score_tag,
+        sentiment,
         polarityTerms,
         agreement: result.agreement
     }
+}
+
+const convertToFullSentiment = (tag) => {
+    let fullSentiment;
+    switch (tag) {
+        case 'P+':
+            fullSentiment = 'very positive'
+            break;
+        case 'P':
+            fullSentiment = 'positive'
+            break;
+        case 'NEU':
+            fullSentiment = 'neutral'
+            break;
+        case 'N':
+            fullSentiment = 'negative'
+            break;
+        case 'N+':
+            fullSentiment = 'very negative'
+            break;
+        default:
+            fullSentiment = 'without emotion'    
+    }
+    return fullSentiment;
 }
