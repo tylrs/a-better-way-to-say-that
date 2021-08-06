fa2b5668266dfcbc9eefe919497a357a7240b744
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-06 09:43:17 -0600

    Remove previous panel render method

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -2,42 +2,6 @@ import './Panel.css'
 import React from 'react'
 
 const Panel = ({info}) => {
-    // let panelContent;
-    // switch (props.type) {
-    //     case '2':
-    //         panelContent = 
-    //             <>
-    //                 <h2>Wow your sentence was:</h2>
-    //                 <p>Positive</p>
-    //                 <hr />
-    //                 <h3>Here are the positive words in your sentence:</h3>
-    //                 <p>happy, bunnies</p>
-    //                 <hr />
-    //                 <h3>How would you like to change your sentence?</h3>
-    //                 <button>More Positive</button>
-    //                 <button>More Negative</button>
-    //                 <button>Random</button>
-    //             </>
-    //         break;   
-    //     case '3': 
-    //         panelContent = 
-    //             <>  
-    //                 <h2>Here's your new sentence:</h2>
-    //                 <p>Bunnies make me sad and feel scared</p>
-    //                 <button>Save this sentence</button>
-    //             </>
-    //         break;
-    //     case '4':
-    //         panelContent = 
-    //             <>
-    //                 <h2>You can now:</h2>
-    //                 <button>Try Again with Same Sentence</button>
-    //                 <button>Change a new sentence</button>
-    //             </>
-    //         break;  
-    //     default: 
-    //         panelContent = <p>Error</p>
-    // }
     const generateWords = (content) => {
         return content.map(word => {
             return (
@@ -45,7 +9,7 @@ const Panel = ({info}) => {
             )   
         })
     }
-    
+
     const panelContent = info.map(({type, content}) => {
         let element;
         switch(type) {
