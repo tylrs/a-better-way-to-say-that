6190b19645722d1c0f7122547c91b2180f5be7bf
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-06 16:35:27 -0600

    Add original index onto newWord

--- src/utils/apicalls.js
+++ src/utils/apicalls.js
@@ -5000,21 +5000,24 @@ export const submitSentence = async (sentence) => {
     // } catch (err) {
     //     throw Error(err)
     // }
-    return cleanSentimentAnalysis(sampleResultNegative)
+    return cleanSentimentAnalysis(sampleResultPositiveAndNegative2)
 }
 
 export const submitWords = async (words, directionChange, wordType) => {
     let newWords = await Promise.all(words.map(async (word) => {
-        console.log("this is one of the selected words>>>>",  word.text)
+        console.log("this is one of the selected words>>>>",  word)
         try {
             // const response = await fetch(`https://dictionaryapi.com/api/v3/references/thesaurus/json/${word.text}?key=9691e0fb-dd4a-4c0f-b4e2-b340a964a4bb`)
             // const result = await response.json()
             // console.log(result)
             let result = sampleThesaurusCrying
-            return findNewWord(result, directionChange, wordType)
+            let newWord = {text: '', originalIndex: word.originalIndex}
+            newWord.text = findNewWord(result, directionChange, wordType)
+            return newWord
         } catch(err) {
             console.log(err)
         }
     }))
     console.log("LOOK AT THE NEW WORDS >>>>", newWords)
+    return newWords
 }
