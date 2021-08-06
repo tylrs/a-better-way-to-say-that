f85d34f556f68bdf00c4a2cfac3b3ccd826f67f6
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-05 22:40:41 -0600

    Commet out code to work with sample data

--- src/utils/apicalls.js
+++ src/utils/apicalls.js
@@ -275,21 +275,21 @@ const sampleResult = {
 }
 
 export const submitSentence = async (sentence) => {
-    const formdata = new FormData();
-    formdata.append("key", "269a10b0ae7249a4022c77a093494421");
-    formdata.append("txt", `${sentence}`);
-    formdata.append("lang", "en");  
-    try {
-        const response = await fetch('https://api.meaningcloud.com/sentiment-2.1', {
-            method: 'POST',
-            body: formdata,
-            redirect: 'follow'
-         })
-        const result = await response.json()
-        console.log(result)
-        return cleanSentimentAnalysis(result)
-    } catch (err) {
-        throw Error(err)
-    }
-    // return cleanSentimentAnalysis(sampleResult)
+    // const formdata = new FormData();
+    // formdata.append("key", "269a10b0ae7249a4022c77a093494421");
+    // formdata.append("txt", `${sentence}`);
+    // formdata.append("lang", "en");  
+    // try {
+    //     const response = await fetch('https://api.meaningcloud.com/sentiment-2.1', {
+    //         method: 'POST',
+    //         body: formdata,
+    //         redirect: 'follow'
+    //      })
+    //     const result = await response.json()
+    //     console.log(result)
+    //     return cleanSentimentAnalysis(result)
+    // } catch (err) {
+    //     throw Error(err)
+    // }
+    return cleanSentimentAnalysis(sampleResult)
 }
