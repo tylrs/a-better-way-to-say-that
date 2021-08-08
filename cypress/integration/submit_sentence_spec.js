8d7820b71129da1cae4a0d0490c7b1983eee53ec
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-08 15:12:41 -0600

    Add tests for mixed sentiment sentence

--- cypress/integration/submit_sentence_spec.js
+++ cypress/integration/submit_sentence_spec.js
@@ -165,7 +165,7 @@ describe('Submit Sentence', () => {
             .contains('continue')
     })
 
-    it.only('Should be able to submit a negative sentence, click the negative option, and see a new negative sentence', () => {
+    it('Should be able to submit a negative sentence, click the negative option, and see a new negative sentence', () => {
         cy.fixture('negative-sentiment.json').then((negativeSentimentAnalysis) => {
             cy.intercept('POST', 'https://api.meaningcloud.com/sentiment-2.1', negativeSentimentAnalysis)
         })
@@ -187,6 +187,91 @@ describe('Submit Sentence', () => {
             .contains('Here is your original sentence:')
             .get('.panel.current-panel > :nth-child(2)')
             .contains('Turing students are smelly and scary')
+            .get('.panel.current-panel > :nth-child(3)')
+            .contains('Here is your new sentence:')
+            .get('.panel.current-panel > :nth-child(4)')
+            .should('have.class', 'negative')
+            .get('.panel.current-panel > .current-panel')
+            .contains('continue')
+    })
+
+    it('Should be able to type in a sentence with both positive and negative words and see those identified', () => {
+        cy.fixture('mixed-sentiment.json').then((mixedSentimentAnalysis) => {
+            cy.intercept('POST', 'https://api.meaningcloud.com/sentiment-2.1', mixedSentimentAnalysis)
+        })
+        cy
+            .get('textarea')
+            .type('I get angry when I see other people having fun')
+            .get('.current-panel > button')
+            .click()
+            .get('h3.result')
+            .contains('neutral')
+            .get('.panel.current-panel > :nth-child(3)')
+            .contains('Positive words:')
+            .get('.panel.current-panel > :nth-child(4)')
+            .contains('fun')
+            .get('.panel.current-panel > :nth-child(5)')
+            .contains('Negative words:')
+            .get('.panel.current-panel > :nth-child(6)')
+            .contains('angry')
+            .get('.current-panel.positive')
+            .contains('positive')
+            .get('.current-panel.negative')
+            .contains('negative')
+    })
+
+    it('Should be able to submit a mixed sentiment sentence, click the positive option, and see a new positive sentence', () => {
+        cy.fixture('mixed-sentiment.json').then((mixedSentimentAnalysis) => {
+            cy.intercept('POST', 'https://api.meaningcloud.com/sentiment-2.1', mixedSentimentAnalysis)
+        })
+        cy.fixture('mixed-thesaurus-word1.json').then((thesaurusResponse) => {
+            cy.intercept('https://dictionaryapi.com/api/v3/references/thesaurus/json/angry?key=9691e0fb-dd4a-4c0f-b4e2-b340a964a4bb', thesaurusResponse).as('thesaurus1')
+        })
+        cy.fixture('mixed-thesaurus-word2.json').then((thesaurusResponse) => {
+            cy.intercept('https://dictionaryapi.com/api/v3/references/thesaurus/json/fun?key=9691e0fb-dd4a-4c0f-b4e2-b340a964a4bb', thesaurusResponse).as('thesaurus2')
+        })
+        cy
+            .get('textarea')
+            .type('I get angry when I see other people having fun')
+            .get('.current-panel > button')
+            .click()
+            .get('.current-panel.positive')
+            .click()
+            .wait(['@thesaurus1', '@thesaurus2'])
+            .get('.panel.current-panel > :nth-child(1)')
+            .contains('Here is your original sentence:')
+            .get('.panel.current-panel > :nth-child(2)')
+            .contains('I get angry when I see other people having fun')
+            .get('.panel.current-panel > :nth-child(3)')
+            .contains('Here is your new sentence:')
+            .get('.panel.current-panel > :nth-child(4)')
+            .should('have.class', 'positive')
+            .get('.panel.current-panel > .current-panel')
+            .contains('continue')
+    })
+
+    it.only('Should be able to submit a mixed sentiment sentence, click the negative option, and see a new negative sentence', () => {
+        cy.fixture('mixed-sentiment.json').then((mixedSentimentAnalysis) => {
+            cy.intercept('POST', 'https://api.meaningcloud.com/sentiment-2.1', mixedSentimentAnalysis)
+        })
+        cy.fixture('mixed-thesaurus-word1.json').then((thesaurusResponse) => {
+            cy.intercept('https://dictionaryapi.com/api/v3/references/thesaurus/json/angry?key=9691e0fb-dd4a-4c0f-b4e2-b340a964a4bb', thesaurusResponse).as('thesaurus1')
+        })
+        cy.fixture('mixed-thesaurus-word2.json').then((thesaurusResponse) => {
+            cy.intercept('https://dictionaryapi.com/api/v3/references/thesaurus/json/fun?key=9691e0fb-dd4a-4c0f-b4e2-b340a964a4bb', thesaurusResponse).as('thesaurus2')
+        })
+        cy
+            .get('textarea')
+            .type('I get angry when I see other people having fun')
+            .get('.current-panel > button')
+            .click()
+            .get('.current-panel.negative')
+            .click()
+            .wait(['@thesaurus1', '@thesaurus2'])
+            .get('.panel.current-panel > :nth-child(1)')
+            .contains('Here is your original sentence:')
+            .get('.panel.current-panel > :nth-child(2)')
+            .contains('I get angry when I see other people having fun')
             .get('.panel.current-panel > :nth-child(3)')
             .contains('Here is your new sentence:')
             .get('.panel.current-panel > :nth-child(4)')
