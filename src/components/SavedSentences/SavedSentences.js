718d392fc2e74c1be4157dd7aa1aab5583199e9a
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-08 10:34:40 -0600

    Render sentence components

--- src/components/SavedSentences/SavedSentences.js
+++ src/components/SavedSentences/SavedSentences.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Sentence from '../Sentence/Sentence'
 
-const SavedSentences = (props) => {
-    const sentences = props.map(sentence => {
-        return (
-            <Sentence />
-        )
-    })
+const SavedSentences = ({savedSentences}) => {
+    const sentences = savedSentences.map(item => 
+        <Sentence sentence={item.sentence} sentenceSentiment={item.sentenceSentiment}/>
+    )
     return (
-        <section>
+        <section className='saved-sentences'>
+            <h2>Your Saved Sentences</h2>
             {sentences}
         </section>
     )
