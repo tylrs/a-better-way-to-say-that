aa7aa6baaec6a0a217fbd43d432d3ad2353d282d
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-08 14:55:59 -0600

    Add negative sentiment tests

--- cypress/integration/submit_sentence_spec.js
+++ cypress/integration/submit_sentence_spec.js
@@ -109,4 +109,90 @@ describe('Submit Sentence', () => {
             .get('.panel.current-panel > .current-panel')
             .contains('continue')
     })
+
+    it('Should be able to type in a sentence with negative words and see those identified', () => {
+        cy.fixture('negative-sentiment.json').then((negativeSentimentAnalysis) => {
+            cy.intercept('POST', 'https://api.meaningcloud.com/sentiment-2.1', negativeSentimentAnalysis)
+        })
+        cy
+            .get('textarea')
+            .type('Bunnies make me feel happy calm and ecstatic')
+            .get('.current-panel > button')
+            .click()
+            .get('h3.result')
+            .contains('negative')
+            .get('.panel.current-panel')
+            .should('not.have', 'Positive words:')
+            .get('.panel.current-panel > :nth-child(3)')
+            .contains('Negative words:')
+            .get('.panel.current-panel > :nth-child(4)')
+            .contains('smelly')
+            .get('.panel.current-panel > :nth-child(5)')
+            .contains('scary')
+            .get('.current-panel.positive')
+            .contains('positive')
+            .get('.current-panel.negative')
+            .contains('negative')
+    })
+
+    it('Should be able to submit a negative sentence, click the positive option, and see a new positive sentence', () => {
+        cy.fixture('negative-sentiment.json').then((negativeSentimentAnalysis) => {
+            cy.intercept('POST', 'https://api.meaningcloud.com/sentiment-2.1', negativeSentimentAnalysis)
+        })
+        cy.fixture('negative-sentiment-thesaurus-word1.json').then((thesaurusResponse) => {
+            cy.intercept('https://dictionaryapi.com/api/v3/references/thesaurus/json/smelly?key=9691e0fb-dd4a-4c0f-b4e2-b340a964a4bb', thesaurusResponse).as('thesaurus1')
+        })
+        cy.fixture('negative-sentiment-thesaurus-word2.json').then((thesaurusResponse) => {
+            cy.intercept('https://dictionaryapi.com/api/v3/references/thesaurus/json/scary?key=9691e0fb-dd4a-4c0f-b4e2-b340a964a4bb', thesaurusResponse).as('thesaurus2')
+        })
+        cy
+            .get('textarea')
+            .type('Turing students are smelly and scary')
+            .get('.current-panel > button')
+            .click()
+            .get('.current-panel.positive')
+            .click()
+            .wait(['@thesaurus1', '@thesaurus2'])
+            .get('.panel.current-panel > :nth-child(1)')
+            .contains('Here is your original sentence:')
+            .get('.panel.current-panel > :nth-child(2)')
+            .contains('Turing students are smelly and scary')
+            .get('.panel.current-panel > :nth-child(3)')
+            .contains('Here is your new sentence:')
+            .get('.panel.current-panel > :nth-child(4)')
+            .should('have.class', 'positive')
+            .get('.panel.current-panel > .current-panel')
+            .contains('continue')
+    })
+
+    it.only('Should be able to submit a negative sentence, click the negative option, and see a new negative sentence', () => {
+        cy.fixture('negative-sentiment.json').then((negativeSentimentAnalysis) => {
+            cy.intercept('POST', 'https://api.meaningcloud.com/sentiment-2.1', negativeSentimentAnalysis)
+        })
+        cy.fixture('negative-sentiment-thesaurus-word1.json').then((thesaurusResponse) => {
+            cy.intercept('https://dictionaryapi.com/api/v3/references/thesaurus/json/smelly?key=9691e0fb-dd4a-4c0f-b4e2-b340a964a4bb', thesaurusResponse).as('thesaurus1')
+        })
+        cy.fixture('negative-sentiment-thesaurus-word2.json').then((thesaurusResponse) => {
+            cy.intercept('https://dictionaryapi.com/api/v3/references/thesaurus/json/scary?key=9691e0fb-dd4a-4c0f-b4e2-b340a964a4bb', thesaurusResponse).as('thesaurus2')
+        })
+        cy
+            .get('textarea')
+            .type('Turing students are smelly and scary')
+            .get('.current-panel > button')
+            .click()
+            .get('.current-panel.negative')
+            .click()
+            .wait(['@thesaurus1', '@thesaurus2'])
+            .get('.panel.current-panel > :nth-child(1)')
+            .contains('Here is your original sentence:')
+            .get('.panel.current-panel > :nth-child(2)')
+            .contains('Turing students are smelly and scary')
+            .get('.panel.current-panel > :nth-child(3)')
+            .contains('Here is your new sentence:')
+            .get('.panel.current-panel > :nth-child(4)')
+            .should('have.class', 'negative')
+            .get('.panel.current-panel > .current-panel')
+            .contains('continue')
+    })
+    
 })
