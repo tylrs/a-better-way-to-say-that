c8cccfab7d6b719eda32f9985d98d26cc54242a3
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-08 14:43:11 -0600

    Add new test for negative option selected

--- cypress/integration/submit_sentence_spec.js
+++ cypress/integration/submit_sentence_spec.js
@@ -76,4 +76,37 @@ describe('Submit Sentence', () => {
             .get('.panel.current-panel > .current-panel')
             .contains('continue')
     })
+
+    it('Should be able to submit a positive sentence, click the negative option, and see a new negative sentence', () => {
+        cy.fixture('positive-sentiment.json').then((positiveSentimentAnalysis) => {
+            cy.intercept('POST', 'https://api.meaningcloud.com/sentiment-2.1', positiveSentimentAnalysis)
+        })
+        cy.fixture('positive-sentiment-thesaurus-word1.json').then((thesaurusResponse) => {
+            cy.intercept('https://dictionaryapi.com/api/v3/references/thesaurus/json/happy?key=9691e0fb-dd4a-4c0f-b4e2-b340a964a4bb', thesaurusResponse).as('thesaurus1')
+        })
+        cy.fixture('positive-sentiment-thesaurus-word2.json').then((thesaurusResponse) => {
+            cy.intercept('https://dictionaryapi.com/api/v3/references/thesaurus/json/calm?key=9691e0fb-dd4a-4c0f-b4e2-b340a964a4bb', thesaurusResponse).as('thesaurus2')
+        })
+        cy.fixture('positive-sentiment-thesaurus-word3.json').then((thesaurusResponse) => {
+            cy.intercept('https://dictionaryapi.com/api/v3/references/thesaurus/json/ecstatic?key=9691e0fb-dd4a-4c0f-b4e2-b340a964a4bb', thesaurusResponse).as('thesaurus3')
+        })
+        cy
+            .get('textarea')
+            .type('Bunnies make me feel happy calm and ecstatic')
+            .get('.current-panel > button')
+            .click()
+            .get('.current-panel.negative')
+            .click()
+            .wait(['@thesaurus1', '@thesaurus2', '@thesaurus3'])
+            .get('.panel.current-panel > :nth-child(1)')
+            .contains('Here is your original sentence:')
+            .get('.panel.current-panel > :nth-child(2)')
+            .contains('Bunnies make me feel happy calm and ecstatic')
+            .get('.panel.current-panel > :nth-child(3)')
+            .contains('Here is your new sentence:')
+            .get('.panel.current-panel > :nth-child(4)')
+            .should('have.class', 'negative')
+            .get('.panel.current-panel > .current-panel')
+            .contains('continue')
+    })
 })
