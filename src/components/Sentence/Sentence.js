6a340d7f45d7302efe09521c39c903393e8cf16f
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-08 10:34:56 -0600

    render props in sentence component

--- src/components/Sentence/Sentence.js
+++ src/components/Sentence/Sentence.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Sentence = () => {
+const Sentence = ({sentence, sentenceSentiment}) => {
     return (
         <article>
-            
+            <h3>{sentence}</h3>
+            <h3>{sentenceSentiment}</h3>
         </article>
     )
 }
