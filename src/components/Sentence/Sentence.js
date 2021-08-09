c66b1aa10974ca2c08a201a1f4cdf812e4c60b20
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-09 10:48:17 -0600

    Add styling to saved sentences section

--- src/components/Sentence/Sentence.js
+++ src/components/Sentence/Sentence.js
@@ -6,9 +6,9 @@ const Sentence = ({sentence, sentenceSentiment}) => {
     return (
         <article className='sentence'>
             <h3>Your Sentence</h3>
-            <p>{sentence}</p>
+            <p className={sentenceSentiment}>{sentence}</p>
             <h4>Your Sentence Sentiment</h4>
-            <p>{sentenceSentiment}</p>
+            <p className={sentenceSentiment}>{sentenceSentiment}</p>
         </article>
     )
 }
