2e206d79cf7379f412a81570516a0ec6cd90548d
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-19 10:51:46 -0500

    Save API keys in variables

--- src/utils/apicalls.js
+++ src/utils/apicalls.js
@@ -2,7 +2,7 @@ import { cleanSentimentAnalysis, findNewWord } from './utils';
 
 export const submitSentence = async (sentence) => {
     const formdata = new FormData();
-    formdata.append("key", "269a10b0ae7249a4022c77a093494421");
+    formdata.append("key", `${process.env.REACT_APP_SENTIMENT}`);
     formdata.append("txt", `${sentence}`);
     formdata.append("lang", "en");  
     try {
@@ -25,7 +25,7 @@ export const submitSentence = async (sentence) => {
 export const submitWords = async (words, directionChange, wordType) => {
     let newWords = await Promise.all(words.map(async (word) => {
         try {
-            const response = await fetch(`https://dictionaryapi.com/api/v3/references/thesaurus/json/${word.text}?key=9691e0fb-dd4a-4c0f-b4e2-b340a964a4bb`)
+            const response = await fetch(`https://dictionaryapi.com/api/v3/references/thesaurus/json/${word.text}?key=${process.env.REACT_APP_DICTIONARY}`)
             if (!response.ok) {
                 throw Error('Thesaurus fetch error')
             } else {
